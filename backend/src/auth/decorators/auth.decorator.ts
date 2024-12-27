4887984f0d8e2a96a5b740102ded47fe57d56c78
ymca <viacheslav.karpunin28@gmail.com>
2024-12-27 16:31:25 +0300

    update project

--- backend/src/auth/decorators/auth.decorator.ts
+++ backend/src/auth/decorators/auth.decorator.ts
@@ -2,7 +2,7 @@ import { applyDecorators, UseGuards } from '@nestjs/common'
 
 import { OnlyAdminGuard } from '../guards/admin.guard'
 import { JwtAuthGuard } from '../guards/jwt.guard'
-import { AdminAndModeratorGuard } from '../guards/admin-doctor.guard'
+import { AdminAndModeratorGuard } from '../guards/admin-moderator.guard'
 import { OnlyModeratorGuard } from '../guards/moderator.guard'
 
 export type TypeRole = 'ADMIN' | 'USER' | 'MODERATOR' | 'A&M' | undefined
---
+++ backend/src/auth/guards/admin-moderator.guard.ts
@@ -0,0 +1,18 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common'
+
+@Injectable()
+export class AdminAndModeratorGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest()
+    const user = request.user
+    if (user.role !== 'ADMIN' && user.role !== 'MODERATOR')
+      throw new ForbiddenException('You have no rights!')
+
+    return user.role === 'ADMIN' || user.role === 'MODERATOR'
+  }
+}
